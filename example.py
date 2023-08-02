e2316f4a6f5190edaeb648fe1e9b2eace1e72b7e
Mehaei <40540969+Mehaei@users.noreply.github.com>
2023-08-02 21:35:34 +0800

    统计示例文件

---
+++ example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: Mehaei
+# @Date:   2023-08-02 20:57:42
+# @Last Modified by:   Mehaei
+# @Last Modified time: 2023-08-02 20:59:28
+# 导入模块
+import os
+
+
+# 入口函数
+def main():
+    msg = """
+        message info
+    """
+    print(msg)
+    print(os.getcwd())
+
+
+# 调用函数
+main()
