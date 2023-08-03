fe05636daf053f715d2f1179ab8b907f92e6d9b1
Mehaei <40540969+Mehaei@users.noreply.github.com>
2023-08-03 14:31:28 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,17 +2,24 @@
 # @Author: Mehaei
 # @Date:   2023-08-02 20:57:42
 # @Last Modified by:   Mehaei
-# @Last Modified time: 2023-08-02 20:59:28
+# @Last Modified time: 2023-08-03 14:28:30
 # 导入模块
 import os
 
 
 # 入口函数
 def main():
+    """this comment"""
+    '''this comment 1'''
+    m1 = """this code"""
+    m1 = '''this code'''
     msg = """
         message info
     """
     print(msg)
+    print(m1)
+    print("""this code also""")
+    print('''this code also''')
     print(os.getcwd())
 
 
